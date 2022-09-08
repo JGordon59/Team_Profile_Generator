8856dc3df8810e2840e7e7c654d9c3498b0b0262
Jonathan Gordon <jonathan.gordon59@gmail.com>
2022-09-08 16:02:19 -0400

    added bootstrap colors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+
 const startHtml = require('./src/startHtml.js');
 const endHtml = require('./src/endHtml.js');
 const infoGeneral = require('./src/questions.js');
